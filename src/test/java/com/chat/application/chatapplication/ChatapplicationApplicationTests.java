b56f8690ce067d0a62e60bff41365dac6e7eb4c0
Md Manawar Iqbal <manawariqbal@Mohmmeds-MacBook-Pro.local>
2025-05-22 02:59:56 +0530

    Initial commit - Spring Boot Chat App

---
+++ src/test/java/com/chat/application/chatapplication/ChatapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.application.chatapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
